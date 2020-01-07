2576ca5a60f18e414e88f918fd92caa46bbdddd7
Noor Mahini <nmahini@berkeley.edu>
2020-01-06 22:13:38 -0800

    draw func on bottom

--- laser.py
+++ laser.py
@@ -7,16 +7,11 @@ class Laser():
 	speed = 600 / DELAY
 	radius = 2
 
-	def __init__(self, position, direction, color_):
+	def __init__(self, position, direction, color):
 		self.pos = position
 		self.dir = direction
 		self.vel = [- x_comp(Laser.speed, self.dir), - y_comp(Laser.speed, self.dir)]
-		self.color = color_
-
-	def draw(self, surface):
-		self.pos[0] = int(self.pos[0])
-		self.pos[1] = int(self.pos[1])
-		pygame.draw.circle(surface, self.color, self.pos, Laser.radius)
+		self.color = color
 
 	def update_pos(self):
 		self.pos[0] += self.vel[0]
@@ -24,6 +19,11 @@ class Laser():
 
 	def out_of_bounds(self):
 		return (self.pos[0] - self.radius < 0) or (self.pos[0] + self.radius > WIDTH) or (self.pos[1] - self.radius < 0) or (self.pos[1] + self.radius > HEIGHT)
+
+	def draw(self, surface):
+		self.pos[0] = int(self.pos[0])
+		self.pos[1] = int(self.pos[1])
+		pygame.draw.circle(surface, self.color, self.pos, Laser.radius)
 
 class Laser_Manager():
 
